7662c79f7b09b2090b985c9a5684603607275ca2
S0LaNGIGA <148493196+S0LaNGIGA@users.noreply.github.com>
2024-07-29 19:53:28 +0300

    Add files via upload

---
+++ io.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types.web_app_info import WebAppInfo
+import json
+
+bot = Bot("6225951340:AAFlD2yrTDBBGWP1Qz2psyZXSTLXVDaiHCk")
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton("Открыть веб страницу", web_app=WebAppInfo(url="https://s0langiga.github.io/TgWEB/")))
+    await message.answer("Привет, заходи в наше приложение!", reply_markup=markup)
+
+@dp.message_handler(content_types=["web_app_data"])
+async def get_data(message: types.Message):
+    res = json.loads(message.web_app_data.data)
+    await message.answer(f'Имя: {res["name"]}\nEmail: {res["email"]}\nТелефон: {res["phone"]}')
+
+
+executor.start_polling(dp)
+
